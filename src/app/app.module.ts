2b53178edc788f7857841e1673adae8c1a7329e7
Jennifer Brito <jennifer.brito3@gmail.com>
2021-04-23 16:45:01 -0300

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { environment } from './../environments/environment.prod';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyAVqxWRiLDbQO6zrQ9jtYV8JzXeA3vI1PY",
+    authDomain: "teste-5b601.firebaseapp.com",
+    projectId: "teste-5b601",
+    storageBucket: "teste-5b601.appspot.com",
+    messagingSenderId: "376363341555",
+    appId: "1:376363341555:web:1938fec2fb47b54d468986"
+
+  }
 };
